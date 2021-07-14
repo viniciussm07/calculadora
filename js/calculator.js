2cce9558657c123829b9ba08689a5c3563ffbdef
Vinicius Martins <vini.soares.martins@gmail.com>
2021-07-13 23:42:13 -0300

    Acertando posições dos botoes e modificando script que vincula a tela da calculadora aos botoes

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,8 @@
-var 
+function screenNum (num) {
+    var screen = document.querySelector('#screen').value
+
+    screen = screen + num;
+    document.querySelector('#screen').value = screen
+
+    console.log(screen);
+}
