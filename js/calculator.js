1eb2a835c23c5a9a4ce41444f225a09adba629d6
Vinicius Martins <vini.soares.martins@gmail.com>
2021-07-13 23:06:03 -0300

    Alterando: divs HTML, e seus respectivos CSSs e retirando javascript

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,1 @@
-function screenNum (num) {
-    var screen = document.querySelector('#screen').value
-
-    screen = screen + num;
-    document.querySelector('#screen').value = screen
-
-    console.log(screen);
-}
+var 
